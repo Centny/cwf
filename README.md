d6cf127395193144fc40a5011d7634cc1bb925d4
Centny <vty@vtys-MacBook-Pro.local>
2015-11-21 00:44:09 +0800

    adding doc and fix build report error.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 clang wide framework
 ===
+
+###Install
+
+```
+yum install libtool autoconf automake make gcc -y
+```
+
+```
+./autogen.sh
+./configure
+make
+make install
+```
+
+###Test
+
+```
+easy_install gcovr
+```
+
+```
+./build.sh
+```
--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 find . -name \*.gcno -type f -delete
 find . -name \*.gcda -type f -delete
 rm -rf reports
+mkdir reports
+mkdir reports/html
+mkdir reports/xml
+mkdir reports/gcov
 if [ "$1" = "" ];then
  ./autogen.sh
  export LIBCWF_CFLAGS="-fprofile-arcs -ftest-coverage"
@@ -17,9 +21,7 @@ export LD_LIBRARY_PATH=`pwd`/build/lib
 cd build/bin
 ./testcwf
 cd ../../
-mkdir reports
-mkdir reports/html
-mkdir reports/xml
+echo "create coverage reports"
 cd src
-gcovr --html --html-details -o ../reports/html/coverage.html -r . -v .
-gcovr --xml -o ../reports/xml/coverage.xml -r . -v .
+gcovr --html --html-details -o ../reports/html/coverage.html -r `pwd` -v `pwd`
+gcovr --xml -o ../reports/xml/coverage.xml -r `pwd` -v `pwd`
