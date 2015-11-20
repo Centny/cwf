636ce4069804086faa3c606e868c43c93ef41702
Centny <vty@vtys-MacBook-Pro.local>
2015-11-21 00:17:53 +0800

    fix gen error.

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,18 @@
+set -e
+touch NEWS README AUTHORS ChangeLog
 if [ `uname` = "Darwin" ];then
  glibtoolize -f -c
 else
  libtoolize -f -c
 fi
-autoheader
-touch NEWS README AUTHORS ChangeLog
-automake --add-missing
-automake
-echo 1
+echo 00
 aclocal -I m4
+echo 11
+autoheader
+echo 22
+automake --add-missing
+echo 33
+automake
+echo 44
 autoconf
+echo 55
